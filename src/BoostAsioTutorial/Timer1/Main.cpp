0942f29bdd7899967bc24e2487f90c82a21b17ee
NanSu <nansu0425@gmail.com>
2024-12-18 17:16:18 +0900

    Added Timer1

---
+++ src/BoostAsioTutorial/Timer1/Main.cpp
@@ -0,0 +1,14 @@
+﻿#include <iostream>
+#include <boost/asio.hpp>
+
+int main()
+{
+    boost::asio::io_context ioContext;
+    
+    boost::asio::steady_timer timer(ioContext, boost::asio::chrono::seconds(5));
+    timer.wait();
+
+    std::cout << "Hello, world!" << std::endl;
+
+    return 0;
+}
