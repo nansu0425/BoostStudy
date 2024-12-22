0803b703c53c5574f3969b818fd95e7cc28884e2
NanSu <nansu0425@gmail.com>
2024-12-22 22:31:20 +0900

    Added Client project

---
+++ src/MmoNetworking/Client/Main.cpp
@@ -0,0 +1,11 @@
+﻿// Client.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <Client/Pch.hpp>
+
+int main()
+{
+    std::cout << "Hello World!\n";
+
+    return 0;
+}
